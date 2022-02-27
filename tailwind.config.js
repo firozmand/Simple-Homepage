2f3508b373cac89494264f6015b87e764d9b830d
Ali Firozmand <firozmand.dev@gmail.com>
2022-02-27 23:24:25 +0330

    first push

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'homebg': "url('./src/background.png')",
+        'redline': "url('./src/fixed-width fixed-height.png')",
+        'downloadicon': "url('./src/icon cool-icon-22.png')",
+        'ticket': "url('./src/icon cool-icon-32.png')",
+        'basket': "url('./src/basket.png')",
+        'ease': "url('./src/ease.png')",
+        'person': "url('./src/persen.png')",
+        'like': "url('./src/like.png')",
+      }
+    },
+  },
+  plugins: [require("daisyui")],
+}
